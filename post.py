7d97c8407a14f803b8c19c03bb358993f3930618
Austin Clemens <austinc@mac.com>
2014-10-12 14:35:49 -0500

    updated pshot function

--- post.py
+++ post.py
@@ -7,14 +7,27 @@ import sys
 
 cgitb.enable() 
 
+data = cgi.FieldStorage()
+player1=data.getfirst('player1')
+player2=data.getfirst('player2')
+player3=data.getfirst('player3')
+player4=data.getfirst('player4')
+player5=data.getfirst('player5')
+
+year=data.getfirst('year')
+
+season=data.getfirst('type')
+
+efficiency=data.getfirst('efficiency')
+
+quarter=data.getfirst('quarter')
+
 con=sqlite3.connect('2013_shot_data.db')
 cur=con.cursor()
 cur.execute("SELECT shot_made_flag FROM test WHERE player_name='Dirk Nowitzki'")
 rows=cur.fetchall()
 
-data = cgi.FieldStorage()
 
-my_param=data.getfirst('a')
 
 print "Content-Type: text/html\n\n"
 print rows
